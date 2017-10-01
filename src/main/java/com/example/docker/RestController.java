cc753cfa9114e47a075d1c2c52cd9f9ae2687b03
Vlad <shevchenkovlad777@gmail.com>
2017-10-02 00:37:08 +0300

    config docker file

--- src/main/java/com/example/docker/RestController.java
+++ src/main/java/com/example/docker/RestController.java
@@ -1,8 +1,10 @@
 package com.example.docker;
 
+
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@org.springframework.web.bind.annotation.RestController
+@Controller
 public class RestController {
 
     @RequestMapping("/")
